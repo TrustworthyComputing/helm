903dd24d36d94fd91ab0b65b52d43e80d75df20f
cgouert <cgouert@udel.edu>
2023-04-26 12:53:35 -0400

    Add capability to convert LUT-based verilog netlists generated by Yosys
    Co-authored-by: Dimitris Mouris <jimouris@udel.edu>

--- src/bin/preprocessor.rs
+++ src/bin/preprocessor.rs
@@ -1,8 +1,11 @@
 use clap::{Arg, Command};
-use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter};
-use std::io::Write;
 use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+
+#[cfg(test)]
+use helm::verilog_parser;
+
 
 fn parse_args() -> (String, String) {
     let matches = Command::new("HELM Preprocessor")
@@ -33,9 +36,7 @@ fn parse_args() -> (String, String) {
     (in_file_name, out_file_name)
 }
 
-fn build_assign_dict(
-    in_file_name: &String
-) -> (HashMap<String, String>, HashSet<String>) {
+fn build_assign_dict(in_file_name: &String) -> HashMap<String, String> {
     let in_file = File::open(in_file_name).expect("Failed to open file");
     let reader = BufReader::new(in_file);
     let mut assign_dict = HashMap::new();
@@ -44,35 +45,47 @@ fn build_assign_dict(
         let line = line.expect("Failed to read line").trim().to_owned();
 
         if line.contains("output") {
-            output_ports.insert(line.split(' ').nth(1).unwrap().trim_end_matches(";").to_string());
-        } else if line.contains("assign") {
+            if line.contains("[") {
+                output_ports.insert(line.split(' ').nth(2).unwrap().trim_end_matches(";").to_string());
+            }
+            else {
+                output_ports.insert(line.split(' ').nth(1).unwrap().trim_end_matches(";").to_string());
+            }
+        } else if line.contains("assign") && !line.contains(">>") {
             let tokens: Vec<&str> = line.split(' ').collect();
             let output = tokens[1].trim_end_matches(";").trim_start_matches('_').trim_end_matches('_').to_string();
             let input = tokens[3].trim_end_matches(";").trim_start_matches('_').trim_end_matches('_').to_string();
-            if output_ports.contains(&output) {
-                assign_dict.insert(input.clone(), output.clone());
+            if output_ports.contains(&output.split('[').nth(0).unwrap().to_string()) {
+                assign_dict.insert(input, output);
+            } else {
+                assign_dict.insert(output, input);
             }
-            assign_dict.insert(output, input);
         }
     }
-    
-    (assign_dict, output_ports)
+
+    assign_dict
 }
 
-fn main() {
-    let (in_file_name, out_file_name) = parse_args();
-    let file = File::open(&in_file_name).expect("Failed to open file");
+
+fn convert_verilog(
+    in_file_name: &String,
+    out_file_name: &String,
+    wire_dict: &HashMap<String, String>
+) {
+    let in_file = File::open(in_file_name).expect("Failed to open file");
     let out_file = File::create(out_file_name).expect("Failed to create file");
 
-    let reader = BufReader::new(&file);
+    let reader = BufReader::new(in_file);
     let mut out_writer = BufWriter::new(out_file);
-    let (wire_dict, outputs) = build_assign_dict(&in_file_name);
-    
-    // let mut wire_dict = HashMap::<String, String>::new();
     let mut wires = Vec::new();
     let mut inputs = Vec::new();
+    let mut outputs = Vec::new();
+    let mut multi_bit_inputs = Vec::new();
+    let mut multi_bit_outputs = Vec::new();
     let mut gates = Vec::new();
-    let supported_gates = vec!["AND", "DFF", "MUX", "NAND", "NOR", "NOT", "OR", "XOR", "XNOR"];
+    let supported_gates = vec![
+        "AND", "DFF", "MUX", "NAND", "NOR", "NOT", "OR", "XOR", "XNOR"
+    ];
 
     let mut lines = reader.lines();
     loop {
@@ -97,13 +110,64 @@ fn main() {
                 }
             },
             "input" => {
-                for i in 1..tokens.len() {
-                    inputs.push(String::from(
-                        tokens[i].trim_matches(',').trim_end_matches(';')
-                    ));
+                if line.contains("[") {
+                    multi_bit_inputs.push("  ".to_string() + &line.to_string());
+                } else {
+                    for i in 1..tokens.len() {
+                        inputs.push(String::from(
+                            tokens[i].trim_matches(',').trim_end_matches(';')
+                        ));
+                    }
                 }
             },
-            "output" | "assign" => {},
+            "output" => {
+                if line.contains("[") {
+                    multi_bit_outputs.push("  ".to_string() + &line.to_string());
+                } else {
+                    for i in 1..tokens.len() {
+                        outputs.push(String::from(
+                            tokens[i].trim_matches(',').trim_end_matches(';')
+                        ));
+                    }
+                }
+            },
+            "assign" => { 
+                if line.contains(">>") { // LUT
+                    let mut lut_line = "lut(".to_owned();
+                    if line.contains('h') {
+                        lut_line += "0x";
+                    }
+                    lut_line += tokens[3].split(['h', 'd']).filter(|s| !s.is_empty()).collect::<Vec<_>>()[1];
+                    lut_line += ", ";
+                    let mut curr_token = tokens[4];
+                    let mut token_idx = 4;
+                    loop {
+                        if !curr_token.contains(">>") && !curr_token.contains("{") {
+                            curr_token = curr_token.trim_start_matches('_').trim_end_matches(',');
+                            curr_token = curr_token.trim_end_matches('_');
+                            if wire_dict.contains_key(curr_token) {
+                                lut_line += &wire_dict[curr_token];
+                            } else {
+                                lut_line += curr_token;
+                            }
+                            lut_line += ", ";
+                        }
+                        token_idx += 1;
+                        curr_token = tokens[token_idx];
+                        if curr_token.contains("};") { // end of lut statement
+                            curr_token = tokens[1].trim_start_matches('_').trim_end_matches('_');
+                            if wire_dict.contains_key(curr_token) {
+                                lut_line += &wire_dict[curr_token];
+                            } else {
+                                lut_line += curr_token;
+                            }
+                            lut_line += ");";
+                            break;
+                        }
+                    }
+                    gates.push(lut_line.to_string());
+                }
+            },
             _ => { // Gate, module
                 // If it's a gate
                 if supported_gates.iter().any(|gate| line.contains(gate)) {
@@ -132,7 +196,6 @@ fn main() {
                                 gate_line += &wire_dict[wire_name];
                             } else {
                                 gate_line += wire_name;
-                                // println!("can't find {:?}", wire_name);
                             }
                             gate_line += ", ";
                         }
@@ -147,7 +210,7 @@ fn main() {
                     gates.push(gate_line.to_string());
                 } else if line.starts_with("module") { // module or end_module
                     out_writer.write((line + "\n").as_bytes()).expect("Failed to write line");
-                }
+                } 
             }
         }
     }
@@ -158,15 +221,24 @@ fn main() {
     out_writer.write(b";\n").expect("Failed to write line");
     
     // Write inputs
-    out_writer.write(b"  input ").expect("Failed to write line");
-    out_writer.write(inputs.join(", ").as_bytes()).expect("Failed to write line");
-    out_writer.write(b";\n").expect("Failed to write line");
+    if !multi_bit_inputs.is_empty() {
+        out_writer.write((multi_bit_inputs.join("\n")+"\n").as_bytes()).expect("Failed to write line");
+    }
+    if !inputs.is_empty() {
+        out_writer.write(b"  input ").expect("Failed to write line");
+        out_writer.write(inputs.join(", ").as_bytes()).expect("Failed to write line");
+        out_writer.write(b";\n").expect("Failed to write line");
+    }
 
     // Write outputs
-    out_writer.write(b"  output ").expect("Failed to write line");
-    // out_writer.write(outputs.iter().join(", ").as_bytes()).expect("Failed to write line");
-    out_writer.write(outputs.iter().cloned().collect::<Vec<_>>().join(", ").as_bytes()).expect("Failed to write line");
-    out_writer.write(b";\n\n").expect("Failed to write line");
+    if !multi_bit_outputs.is_empty() {
+        out_writer.write((multi_bit_outputs.join("\n")+"\n").as_bytes()).expect("Failed to write line");
+    }
+    if !outputs.is_empty() {
+        out_writer.write(b"  output ").expect("Failed to write line");
+        out_writer.write(outputs.join(", ").as_bytes()).expect("Failed to write line");
+        out_writer.write(b";\n").expect("Failed to write line");
+    }
     
     // Write gates
     for gate in gates {
@@ -174,5 +246,43 @@ fn main() {
     }
 
     out_writer.write(b"\nendmodule\n").expect("Failed to write line");
+}
+
+fn main() {
+    let (in_file_name, out_file_name) = parse_args();
+
+    let wire_dict = build_assign_dict(&in_file_name);
+    convert_verilog(&in_file_name, &out_file_name, &wire_dict);
+}
+
+#[test]
+fn test_preprocessor() {
+    let in_file_name = String::from("verilog-files/s27.v");
+    let out_file_name = String::from("verilog-files/s27.out.v");
+
+    let wire_dict = build_assign_dict(&in_file_name);
+
+    convert_verilog(&in_file_name, &out_file_name, &wire_dict);
+
+    let (gates, wire_map, inputs, dff_outputs,  _) = 
+        verilog_parser::read_verilog_file("verilog-files/s27.out.v");
+
+    assert_eq!(gates.len(), 14);
+    assert_eq!(wire_map.len(), 14);
+    assert_eq!(inputs.len() - dff_outputs.len(), 6); // these are the true inputs
+
+    let in_file_name = String::from("verilog-files/8bit-adder-lut.v");
+    let out_file_name = String::from("verilog-files/8bit-adder-lut.out.v");
+
+    let wire_dict = build_assign_dict(&in_file_name);
+
+    convert_verilog(&in_file_name, &out_file_name, &wire_dict);
+
+    // let (gates, wire_map, inputs, dff_outputs,  _) = 
+    //     verilog_parser::read_verilog_file("verilog-files/s27.out.v");
+
+    // assert_eq!(gates.len(), 14);
+    // assert_eq!(wire_map.len(), 14);
+    // assert_eq!(inputs.len() - dff_outputs.len(), 6); // these are the true inputs
 
 }
